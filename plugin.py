ec6ca011a416b21728b4f5ed403eca85c4615dbf
Branko Grubić <bitlord0xff@gmail.com>
2015-07-03 08:13:00 +0200

    Remove unneeded length check

--- plugin.py
+++ plugin.py
@@ -724,7 +724,7 @@ class Fedora(callbacks.Plugin):
         recip = recip.strip().split()[-1]
 
         # Exclude 'c++', 'g++' or 'i++' (c,g,i), issue #30
-        if len(recip) == 1 and recip.lower() in ['c','g','i']:
+        if recip.lower() in ['c','g','i']:
             return
         
         increment = direction == '++' # If not, then it must be decrement
